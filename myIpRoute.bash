b8b62e8640396fcb46ba7103a2e177f7e66cc9ec
whitfield Gregg <whitgregg@hotmail.com>
2025-08-11 08:47:59 -0700

    just before switching to Kilted

---
+++ myIpRoute.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##### This script is used to set up the IP route for the vqwPi2
+
+##### Set the IP address of the vqwPi2
+#ip addr add dev eth0 local 192.168.42.2 global
+
+##### set the route to other devices on the 192.168.42.* network
+#ip route add to 192.168.42.0/24 dev eth0 metric mtu 1500 static 
+
+#ip route 
